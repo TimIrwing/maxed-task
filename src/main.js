adf22e2f1b991c6da011cf6872380eb88695b68e
Tim <timirwing@gmail.com>
2019-11-22 15:27:50 +0300

    add and setup Vuex

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from '@/App.vue';
 import vuetify from '@/plugins/vuetify';
+import store from '@/store';
 
 Vue.use(VueRouter);
 Vue.config.productionTip = false;
 
 new Vue({
   vuetify,
+  store,
   render: (h) => h(App),
 }).$mount('#app');
---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+import mutations from '@/store/mutations';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+  },
+  mutations,
+});
