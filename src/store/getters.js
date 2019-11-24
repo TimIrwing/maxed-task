dd2b4ee77a02417b1f0e2e402ef36d0faef75226
Tim <timirwing@gmail.com>
2019-11-24 12:29:08 +0300

    remove useless getter

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,4 +1,3 @@
 export default {
   getCourseLessons: (state) => (id) => state.lessons.filter((lesson) => lesson.courseId === id),
-  getLesson: (state) => (id) => state.lessons.find((lesson) => lesson.id === id),
 };
--- src/views/Lesson.vue
+++ src/views/Lesson.vue
@@ -1,7 +1,7 @@
 <template>
   <v-card class="ma-12">
     <v-tabs v-model="tab"
-centered>
+            centered>
       <v-tab>
         Тесты
       </v-tab>
@@ -22,11 +22,6 @@ export default {
     tab: 0,
     paths: ['tests', 'stats'],
   }),
-  computed: {
-    lesson() {
-      return this.$store.getters.getLesson(this.$route.params.id);
-    },
-  },
   watch: {
     tab() {
       this.$router.push({ name: this.paths[this.tab] });
